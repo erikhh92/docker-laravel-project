733f356b3870f9c05fd3ddce2fe83f31b9530431
Eric Molina <eric.molina@rnaiberica.com>
2021-05-12 08:31:07 +0200

    code cleanup

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PostsRequest;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -10,14 +10,8 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-
         $posts = Auth::user()->posts()->paginate(5);
 
         if(request()->page > $posts->lastPage()) {
@@ -29,40 +23,18 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return Inertia::render('Posts/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(PostsRequest $request)
+    public function store(PostRequest $request)
     {
-        /*$request->validate([
-            'title' => ['required', 'max:250', 'min:5'],
-            'text' => ['required', 'min:20', 'max:500']
-        ]);*/
-
         Auth::user()->posts()->create($request->all());
 
         return redirect()->route('posts.index')->with('status', ['type' => 'success', 'message' => __('post stored successfully')]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function show(Post $post)
     {
         return Inertia::render('Posts/Show', [
@@ -70,12 +42,6 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Post $post)
     {
         return Inertia::render('Posts/Edit', [
@@ -83,31 +49,13 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(PostsRequest $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        /*$request->validate([
-            'title' => ['required', 'max:250', 'min:5'],
-            'text' => ['required', 'min:20', 'max:500']
-        ]);*/
-
         $post->update($request->all());
 
         return redirect()->route('posts.index')->with('status', ['type' => 'success', 'message' => __('post updated successfully')]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Post $post)
     {
         $post->delete();

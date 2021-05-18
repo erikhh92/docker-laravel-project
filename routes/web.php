8c8618b575845d066dcd2f512f103f2ea461a9d5
Edgar <edgargomeztorras@gmail.com>
2021-05-18 16:39:14 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Inertia\Inertia;
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FileController;
 
 /*
 |--------------------------------------------------------------------------
